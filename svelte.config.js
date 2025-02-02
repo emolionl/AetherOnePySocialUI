31e5df8aa086c12483e9a1dc13d0e7207da584f2
davorradic <45642322+emolionl@users.noreply.github.com>
2025-02-02 14:53:47 +0100

    update config file

--- svelte.config.js
+++ svelte.config.js
@@ -5,13 +5,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   kit: {
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: '../build',
-      assets: '../build',
+      pages: 'build',
+      assets: 'build',
       fallback: 'index.html',
-      precompress: false,
-      strict: true
+      precompress: false
     })
   },
   preprocess: vitePreprocess()
